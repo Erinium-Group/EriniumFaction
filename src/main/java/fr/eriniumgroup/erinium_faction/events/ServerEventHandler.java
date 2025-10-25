d852ae040c359efe35f81c67e0c04747432c0d4f
Blaackknight <dragclover@gmail.com>
2025-10-25 19:51:46 +0200

    Add chat formatting for faction and ally messages, and update economy command messages

--- src/main/java/fr/eriniumgroup/erinium_faction/events/ServerEventHandler.java
+++ src/main/java/fr/eriniumgroup/erinium_faction/events/ServerEventHandler.java
@@ -3,38 +3,32 @@ package fr.eriniumgroup.erinium_faction.events;
 import fr.eriniumgroup.erinium_faction.core.EFC;
 import fr.eriniumgroup.erinium_faction.core.faction.Faction;
 import fr.eriniumgroup.erinium_faction.core.faction.FactionManager;
-import fr.eriniumgroup.erinium_faction.core.faction.Rank;
 import fr.eriniumgroup.erinium_faction.core.rank.EFRManager;
 import net.minecraft.network.chat.Component;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.ServerChatEvent;
 
+import java.util.Objects;
+
 @EventBusSubscriber(modid = EFC.MODID)
 public class ServerEventHandler {
 
     @SubscribeEvent
-    public static void onChat(ServerChatEvent e){
+    public static void onChat(ServerChatEvent e) {
         e.setCanceled(true);
 
         EFRManager.Rank r = EFRManager.get().getPlayerRank(e.getPlayer().getUUID());
         Faction f = FactionManager.getFactionOf(e.getPlayer().getUUID());
 
-        String formattedText = (r != null ? r.displayName : "§7Membre")
-                + " " + (f != null ? f.getName() : "§aWilderness")
-                + " §r| " + e.getPlayer().getDisplayName().getString()
-                + "§r: " + e.getMessage().getString();
+        Component formattedText = Component.translatable("erinium_faction.chat.global_format", r != null ? r.displayName : "§7Membre", f != null ? f.getName() : "§aWilderness", e.getPlayer().getDisplayName(), e.getMessage());
 
-        // Convertir & en § seulement pour les codes valides
         String coloredText = translateColorCodes(formattedText);
 
-        e.getPlayer().getServer().getPlayerList().broadcastSystemMessage(Component.literal(coloredText), false);
+        Objects.requireNonNull(e.getPlayer().getServer()).getPlayerList().broadcastSystemMessage(Component.literal(coloredText), false);
     }
 
-    private static String translateColorCodes(String text) {
-        // Codes de couleur valides : 0-9, a-f, k-o, r
-        // 0-9, a-f = couleurs
-        // k = obfusqué, l = gras, m = barré, n = souligné, o = italique, r = reset
-        return text.replaceAll("&([0-9a-fk-or])", "§$1");
+    private static String translateColorCodes(Component text) {
+        return text.getString().replaceAll("&([0-9a-fk-or])", "§$1");
     }
 }
