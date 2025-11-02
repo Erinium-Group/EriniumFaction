847f3de8e1e7771280c04ef3a855b4b123183763
Blaackknight <dragclover@gmail.com>
2025-11-02 23:06:20 +0100

    Refactor silver and titanium processing JSON files and update block classes

--- src/main/java/fr/eriniumgroup/erinium_faction/common/block/DeepslateSilverOreBlock.java
+++ src/main/java/fr/eriniumgroup/erinium_faction/common/block/DeepslateSilverOreBlock.java
@@ -1,15 +1,15 @@
 package fr.eriniumgroup.erinium_faction.common.block;
 
 import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.block.SoundType;
 
-public class DeepslateSilverOreBlock extends DropExperienceBlock {
+public class DeepslateSilverOreBlock extends Block {
     public DeepslateSilverOreBlock() {
         super(
-            UniformInt.of(1, 3),
             BlockBehaviour.Properties.of()
                 .mapColor(MapColor.DEEPSLATE)
                 .sound(SoundType.DEEPSLATE)
